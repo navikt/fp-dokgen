37264d7b5cf8ca9bc53f3035f51978dff53f0dc2
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2020-10-12 09:41:45 +0200

    TFP-3697: Justeringer etter flytting av POM-fil.

--- collect-contracts.sh
+++ collect-contracts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir build/tmp
+mkdir tmp
 
 templatePath="content/templates/"
 
@@ -8,6 +8,6 @@ for file in $templatePath*/ ; do
   if [[ -d "$file" && ! -L "$file" ]]; then
     dir=${file%*/}
     dir=${dir##*/}
-    cp $templatePath$dir/schema.json "build/tmp/$dir.json"
+    cp $templatePath$dir/schema.json "tmp/$dir.json"
   fi;
 done
