0886a38f76bef66ca10288f3bf6d80c8f66a4199
anjaaalerud <anja.gotesen.aalerud@nav.no>
2020-10-13 14:59:26 +0200

    TFP-3697 Opprettet et første utkast for formatering av innvilgelse engangsstønad for alle varianter på bokmål og nynorsk. Opprettet unitest for noen varianter.

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/EngangsstønadInnvilgelseTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/EngangsstønadInnvilgelseTest.java
@@ -1,10 +1,9 @@
 package no.nav.foreldrepenger.dokgen.test.templates;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import no.nav.foreldrepenger.dokgen.test.support.TemplateTestService;
 import org.junit.Test;
 
-import no.nav.foreldrepenger.dokgen.test.support.TemplateTestService;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class EngangsstønadInnvilgelseTest {
 
@@ -12,20 +11,42 @@ public class EngangsstønadInnvilgelseTest {
     private static final TemplateTestService templateTestService = new TemplateTestService();
 
     @Test
-    public void skal_generere_brev_på_bokmål() throws Exception {
+    public void skal_generere_brev_for_innvilgelse_ESførstegang_på_bokmål() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nb", "test");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nb", "testInnvFB");
 
         // Assert
         assertThat(resultat).isEqualTo(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_nb.txt"));
     }
 
     @Test
-    public void skal_generere_brev_på_nynorsk() throws Exception {
+    public void skal_generere_brev_for_innvilgelse_ESførstegang_på_nynorsk() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nn", "test");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nn", "testInnvFB");
 
         // Assert
         assertThat(resultat).isEqualTo(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_nn.txt"));
     }
+
+    //Varianter av samme mal
+    @Test
+    public void skal_generere_brev_for_død_på_bokmål() throws Exception {
+        // Act
+        String expectedFilname = TEMPLATE_NAME + "-død";
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nb", "testDød");
+
+        // Assert
+        assertThat(resultat).isEqualTo(templateTestService.getExpectedResult(TEMPLATE_NAME, expectedFilname + "_nb.txt"));
+    }
+
+    @Test
+    public void skal_generere_brev_for_medhold_på_bokmål() throws Exception {
+        // Act
+        String expectedFilname = TEMPLATE_NAME + "-medhold";
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, "nb", "testMedhold");
+
+        // Assert
+        assertThat(resultat).isEqualTo(templateTestService.getExpectedResult(TEMPLATE_NAME, expectedFilname + "_nb.txt"));
+    }
+
 }
