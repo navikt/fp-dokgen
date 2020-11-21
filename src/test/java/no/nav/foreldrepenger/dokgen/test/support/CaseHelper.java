8c8d7d3af67ed74745a8c39388dacaa554f705f0
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2020-11-21 16:32:40 +0100

    TFP-3847 Lagt til ny mal og enhetstest for avslag engangsstønad. (#46)

---
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/support/CaseHelper.java
@@ -0,0 +1,22 @@
+package no.nav.foreldrepenger.dokgen.test.support;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CaseHelper implements Helper<Object> {
+    @Override
+    public Object apply(Object caseKonstant, Options options) throws IOException {
+        Object konstant = options.hash.isEmpty() ? caseKonstant : options.hash;
+        Map<String, Object> model = (Map<String, Object>) options.context.model();
+        Object condition_variable = model.get("__condition_variable");
+        if (konstant.equals(condition_variable)) {
+            Integer antall = (Integer) model.get("__condition_fulfilled");
+            model.put("__condition_fulfilled", ++antall);
+            return options.fn();
+        }
+        return options.inverse();
+    }
+}
