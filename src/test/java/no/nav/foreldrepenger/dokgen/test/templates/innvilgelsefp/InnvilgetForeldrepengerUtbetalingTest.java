85def437530752e4a53e9520047793b4b8682ed0
anjaaalerud <anja.gotesen.aalerud@nav.no>
2021-05-07 16:08:40 +0200

    TFP-4272 Opprettet tester for undermalene avslag og gradering som tilhører innvilget foreldrepenger

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/innvilgelsefp/InnvilgetForeldrepengerUtbetalingTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/innvilgelsefp/InnvilgetForeldrepengerUtbetalingTest.java
@@ -8,34 +8,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class InnvilgetForeldrepengerUtbetalingTest {
     private static final String TEMPLATE_NAME = "innvilget-foreldrepenger";
     private static final String TEMPLATE_PATH = "/utbetaling_";
+    private static final String EXPECTED_PREFIX = "utbetaling/innvilget-foreldrepenger";
     private static final TemplateTestService templateTestService = new TemplateTestService();
 
 
     @Test
     public void undermal_utbetaling_ingen_refusjon_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_ingen_refusjon");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "utbetaling/førstegangsbehandling_ingen_refusjon");
 
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_utbetaling_ingen_refusjon_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, EXPECTED_PREFIX + "_utbetaling_ingen_refusjon_nb.txt"));
     }
 
     @Test
     public void undermal_utbetaling_full_refusjon_rettigheter_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_full_refusjon");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "utbetaling/førstegangsbehandling_full_refusjon");
 
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_utbetaling_full_refusjon_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, EXPECTED_PREFIX + "_utbetaling_full_refusjon_nb.txt"));
     }
 
     @Test
     public void undermal_utbetaling_delvis_refusjon_rettigheter_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_delvis_refusjon");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "utbetaling/førstegangsbehandling_delvis_refusjon");
 
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_utbetaling_delvis_refusjon_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, EXPECTED_PREFIX + "_utbetaling_delvis_refusjon_nb.txt"));
     }
 
 
