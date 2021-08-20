271e0407aebf87e0a01adeaa07e59dc2faf5c110
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2021-08-20 16:05:28 +0200

    Bedre test revurdering (#250)
    * Flyttet førstegangsbehandling testinput-filer
    
    * Work-in-progress
    
    * Innvilget-Foreldrepenger: Lagt til test revurdering
    
    Lagt til test av revurdering for innvilgelse foreldrepegner og forbedret struktur og navngivning
    
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/innvilgelsefp/InnvilgetForeldrepengerTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/innvilgelsefp/InnvilgetForeldrepengerTest.java
@@ -14,35 +14,63 @@ public class InnvilgetForeldrepengerTest {
     @Test
     public void førstegangsbehandling_uten_gradering_og_avslag_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_automatisk_ingen_gradering_ingen_avslag");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "forstegangsbehandling/automatisk_ingen_gradering_ingen_avslag");
 
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_template_forstegangsbehandling_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME,  "forstegangsbehandling/forstegangsbehandling_uten_gradering_avslag_nb.txt"));
     }
 
     @Test
     public void førstegangsbehandling_med_avslag_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_med_avslag");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "forstegangsbehandling/med_avslag_periode");
 
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_template_forstegangsbehandling_med_avslag_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME,  "forstegangsbehandling/forstegangsbehandling_med_avslag_periode_nb.txt"));
     }
 
     @Test
     public void førstegangsbehandling_med_fritekst_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_med_fritekst");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "forstegangsbehandling/med_fritekst");
         // Assert
-        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_template_forstegangsbehandling_med_fritekst_nb.txt"));
+        assertThat(resultat).isEqualToNormalizingWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME,  "forstegangsbehandling/forstegangsbehandling_med_fritekst_nb.txt"));
     }
 
     @Test
     public void førstegangsbehandling_med_flere_arbeidsgivere_og_gradering_nb() throws Exception {
         // Act
-        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "førstegangsbehandling_gradering_flere_arbgivere_og_gradering");
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "forstegangsbehandling/gradering_flere_arbgivere_og_gradering");
 
         // Assert
-        assertThat(resultat).isEqualToIgnoringWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME,  TEMPLATE_NAME + "_førstegangsbehandling_gradering_nb.txt"));
+        assertThat(resultat).isEqualToIgnoringWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME,  "forstegangsbehandling/forstegangsbehandling_gradering_nb.txt"));
     }
+
+    @Test
+    public void revurdering_foreldrepenger_endret_endring_i_uttak() throws Exception {
+        // Act
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "revurdering/foreldrepenger_endret_endring_i_uttak");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, "revurdering/revurdering_foreldrepenger_endret_nb.txt"));
+    }
+
+    @Test
+    public void revurdering_foreldrepenger_innvilget_endring_i_uttak() throws Exception {
+        // Act
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "revurdering/foreldrepenger_innvilget_endring_i_uttak");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, "revurdering/revurdering_foreldrepenger_innvilget_endring_i_uttak_nb.txt"));
+    }
+
+    @Test
+    public void revurdering_foreldrepenger_endret_endring_i_beregning() throws Exception {
+        // Act
+        String resultat = templateTestService.compileTemplateWithTestData(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "revurdering/foreldrepenger_endret_endring_i_beregning");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(templateTestService.getExpectedResult(TEMPLATE_NAME, "revurdering/revurdering_foreldrepenger_endret_endring_i_beregning_nb.txt"));
+    }
+
 }
