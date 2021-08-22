ea2fc5d35969b9de34a914fb02cb86a3d97786c7
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-08-22 22:05:01 +0200

    Inline method

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/VarselOmRevurderingTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/VarselOmRevurderingTest.java
@@ -12,86 +12,118 @@ public class VarselOmRevurderingTest {
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_ES_kode_ANNET_på_bokmål() throws Exception {
-        compileTextAndCompareWithExpected("nb", "test_es_ANNET", "_es_ANNET_nb.txt", TEMPLATE_NAME);
-    }
-
-    private void compileTextAndCompareWithExpected(String nb, String test_es_annet, String s, String templateName) throws Exception {
         // Act
-        String resultat = compile(templateName, TEMPLATE_PATH, nb, test_es_annet);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_es_ANNET");
 
         // Assert
-        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(templateName, templateName + s));
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_es_ANNET_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_ES_kode_ANNET_på_engelsk() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("en", "test_es_ANNET", "_es_ANNET_en.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "en", "test_es_ANNET");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_es_ANNET_en.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_ES_kode_BARNIKKEREG_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_es_BARNIKKEREG", "_es_BARNIKKEREG_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_es_BARNIKKEREG");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_es_BARNIKKEREG_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_ES_kode_BARNIKKEREG_på_engelsk() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("en", "test_es_BARNIKKEREG", "_es_BARNIKKEREG_en.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "en", "test_es_BARNIKKEREG");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_es_BARNIKKEREG_en.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_AKTIVITET_flere_opplysninger_og_2barn_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_fp_AKTIVITET_flere_opplysninger_2barn", "_fp_AKTIVITET_flere_opplysninger_2barn_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_fp_AKTIVITET_flere_opplysninger_2barn");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_AKTIVITET_flere_opplysninger_2barn_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_AKTIVITET_flere_opplysninger_og_2barn_på_nynorsk() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nn", "test_fp_AKTIVITET_flere_opplysninger_2barn", "_fp_AKTIVITET_flere_opplysninger_2barn_nn.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nn", "test_fp_AKTIVITET_flere_opplysninger_2barn");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_AKTIVITET_flere_opplysninger_2barn_nn.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_IKKEOPPTJENT_ikke_flere_opplysninger_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_fp_IKKEOPPTJENT_ikke_flere_opplysninger", "_fp_IKKEOPPTJENT_ikke_flere_opplysninger_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_fp_IKKEOPPTJENT_ikke_flere_opplysninger");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_IKKEOPPTJENT_ikke_flere_opplysninger_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_JOBBFULLTID_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_fp_JOBBFULLTID", "_fp_JOBBFULLTID_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_fp_JOBBFULLTID");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_JOBBFULLTID_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_JOBBFULLTID_på_nynorsk() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nn", "test_fp_JOBBFULLTID", "_fp_JOBBFULLTID_nn.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nn", "test_fp_JOBBFULLTID");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_JOBBFULLTID_nn.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_FP_kode_ANNET_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_fp_ANNET", "_fp_ANNET_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_fp_ANNET");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_fp_ANNET_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_SVP_kode_JOBBFULLTID_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_svp_JOBBFULLTID", "_svp_JOBBFULLTID_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_svp_JOBBFULLTID");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_svp_JOBBFULLTID_nb.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_SVP_kode_JOBBFULLTID_på_nynorsk() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nn", "test_svp_JOBBFULLTID", "_svp_JOBBFULLTID_nn.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nn", "test_svp_JOBBFULLTID");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_svp_JOBBFULLTID_nn.txt"));
     }
 
     @Test
     public void skal_generere_brev_for_varsel_om_revurdering_SVP_kode_JOBB6MND_på_bokmål() throws Exception {
         // Act
-        compileTextAndCompareWithExpected("nb", "test_svp_JOBB6MND", "_svp_JOBB6MND_nb.txt", TEMPLATE_NAME);
+        String resultat = compile(TEMPLATE_NAME, TEMPLATE_PATH, "nb", "test_svp_JOBB6MND");
+
+        // Assert
+        assertThat(resultat).isEqualToIgnoringWhitespace(getExpectedResult(TEMPLATE_NAME, TEMPLATE_NAME + "_svp_JOBB6MND_nb.txt"));
     }
 }
