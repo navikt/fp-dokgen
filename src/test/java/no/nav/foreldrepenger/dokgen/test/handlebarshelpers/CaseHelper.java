a7f6f59dac618bfa116a270c3044119ec7817f3b
Michal J. Sladek <mrsladek@users.noreply.github.com>
2021-09-17 13:24:30 +0200

    TFP-4608, TFP-4618: Fikset tom dato, Støtte for array i en case expresion(#301)
    * TFP-4618: Støtte for array i en case expresion.
    * Merged inn bugfix/TFP-4608 fikset tom dato

--- src/test/java/no/nav/foreldrepenger/dokgen/test/handlebarshelpers/CaseHelper.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/handlebarshelpers/CaseHelper.java
@@ -4,19 +4,33 @@ import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class CaseHelper implements Helper<Object> {
+
+    private static final String CONDITION_VARIABLE = "__condition_variable";
+    private static final String CONDITION_FULFILLED = "__condition_fulfilled";
+
     @Override
     public Object apply(Object caseKonstant, Options options) throws IOException {
         Object konstant = options.hash.isEmpty() ? caseKonstant : options.hash;
         Map<String, Object> model = (Map<String, Object>) options.context.model();
-        Object condition_variable = model.get("__condition_variable");
-        if (konstant.equals(condition_variable)) {
-            Integer antall = (Integer) model.get("__condition_fulfilled");
-            model.put("__condition_fulfilled", ++antall);
+        Object condition_variable = model.get(CONDITION_VARIABLE);
+        if (caseKonstant instanceof Iterable) {
+            if (((List) caseKonstant).contains(condition_variable)) {
+                incrementConditionFulfilledCounter(model);
+                return options.fn();
+            }
+        } else if (konstant.equals(condition_variable)) {
+            incrementConditionFulfilledCounter(model);
             return options.fn();
         }
         return options.inverse();
+    }
+
+    private void incrementConditionFulfilledCounter(final Map<String, Object> model) {
+        Integer antall = (Integer) model.get(CONDITION_FULFILLED);
+        model.put(CONDITION_FULFILLED, ++antall);
     }
 }
