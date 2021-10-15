74ab2b60a5e15e216407b4544588c69982229795
Michal J. Sladek <michal.sladek@nav.no>
2021-10-15 15:44:31 +0200

    Opphør foreldrepenger første utkast

---
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/ForeldrepengerOpphørTest.java
@@ -0,0 +1,39 @@
+package no.nav.foreldrepenger.dokgen.test.templates;
+
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.compileContent;
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.getExpected;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.dokgen.test.support.Brevmal;
+import no.nav.foreldrepenger.dokgen.test.support.Språk;
+
+public class ForeldrepengerOpphørTest {
+    private static final Brevmal BREVMAL = Brevmal.FORELDREPENGER_OPPHØR;
+
+    @Test
+    public void skal_generere_foreldrepenger_opphør_brevet_med_de_fleste_avslagsårsakene_på_bokmål() throws Exception {
+        var content = compileContent(BREVMAL, Språk.BOKMÅL, "test_mange");
+        var expected = getExpected(BREVMAL, "foreldrepenger-opphør_mange_nb.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Disabled
+    @Test
+    public void skal_generere_foreldrepenger_opphør_brevet_med_de_fleste_avslagsårsakene_på_nynorsk() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "test_mange");
+        var expected = getExpected(BREVMAL, "foreldrepenger-opphør_mange_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Disabled
+    @Test
+    public void skal_generere_foreldrepenger_opphør_brevet_med_de_fleste_avslagsårsakene_på_engelsk() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "test_mange");
+        var expected = getExpected(BREVMAL, "foreldrepenger-opphør_mange_en.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+}
