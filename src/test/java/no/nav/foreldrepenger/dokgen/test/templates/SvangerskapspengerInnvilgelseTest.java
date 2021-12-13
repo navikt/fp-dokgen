0571937618b64495fd1e6eca0a6f71750092a8c3
Jan Erik Johnsen <jejohnsen@users.noreply.github.com>
2021-12-13 10:46:52 +0100

    TFP-4696: Nynorsk versjon av SVP innvilgelse. (#451)

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/SvangerskapspengerInnvilgelseTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/SvangerskapspengerInnvilgelseTest.java
@@ -21,6 +21,20 @@ public class SvangerskapspengerInnvilgelseTest {
     }
 
     @Test
+    public void svp_innvilgelse_fgb_at_fl_sn_nn() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "test_fgb_at_fl_sn");
+        var expected = getExpected(BREVMAL, "fgb_at_fl_sn_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void svp_innvilgelse_fgb_at_fl_sn_en_skal_gi_nb() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "test_fgb_at_fl_sn");
+        var expected = getExpected(BREVMAL, "fgb_at_fl_sn_nb.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
     public void svp_innvilgelse_fgb_sn_nb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "test_fgb_sn");
         var expected = getExpected(BREVMAL, "fgb_sn_nb.txt");
