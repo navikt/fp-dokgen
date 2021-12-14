e7099fb93831cd9e95d295cf83e78ea49c61fe71
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2021-12-14 14:11:50 +0100

    TFP-1024 Opphørsbrev svangerskapspenger. (#453)
    Lagt inn nynorsk og engelsk for svangerskapspenger opphør. Jeg har også endret opphørstekster "hadde ikke rett" til "har ikke rett".

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/SvangerskapspengerOpphørTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/SvangerskapspengerOpphørTest.java
@@ -12,30 +12,86 @@ public class SvangerskapspengerOpphørTest {
     private static final Brevmal BREVMAL = Brevmal.SVANGERSKAPSPENGER_OPPHØR;
 
     @Test
-    public void bruker_har_mottatt_sykepenger() throws Exception {
+    public void bruker_har_mottatt_sykepenger_nb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "mottatt_sykepenger");
         var expected = getExpected(BREVMAL, "mottatt_sykepenger_nb.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    public void test_søker_død() throws Exception {
+    public void bruker_har_mottatt_sykepenger_nn() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "mottatt_sykepenger");
+        var expected = getExpected(BREVMAL, "mottatt_sykepenger_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_har_mottatt_sykepenger_en() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "mottatt_sykepenger");
+        var expected = getExpected(BREVMAL, "mottatt_sykepenger_en.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void test_søker_død_nb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "bruker_dod");
         var expected = getExpected(BREVMAL, "bruker_er_dod_nb.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    public void bruker_er_ikke_medlem() throws Exception {
+    public void test_søker_død_nn() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "bruker_dod");
+        var expected = getExpected(BREVMAL, "bruker_er_dod_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void test_søker_død_en() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "bruker_dod");
+        var expected = getExpected(BREVMAL, "bruker_er_dod_en.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_er_ikke_medlem_nb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "medl_1024");
         var expected = getExpected(BREVMAL, "medl_1024_nb.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    public void bruker_er_ikke_gravid() throws Exception {
+    public void bruker_er_ikke_medlem_nn() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "medl_1024");
+        var expected = getExpected(BREVMAL, "medl_1024_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_er_ikke_medlem_en() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "medl_1024");
+        var expected = getExpected(BREVMAL, "medl_1024_en.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_er_ikke_gravid_nb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "bruker_ikke_gravid");
         var expected = getExpected(BREVMAL, "bruker_ikke_gravid_nb.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_er_ikke_gravid_nn() throws Exception {
+        var content = compileContent(BREVMAL, Språk.NYNORSK, "bruker_ikke_gravid");
+        var expected = getExpected(BREVMAL, "bruker_ikke_gravid_nn.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void bruker_er_ikke_gravid_en() throws Exception {
+        var content = compileContent(BREVMAL, Språk.ENGELSK, "bruker_ikke_gravid");
+        var expected = getExpected(BREVMAL, "bruker_ikke_gravid_en.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
