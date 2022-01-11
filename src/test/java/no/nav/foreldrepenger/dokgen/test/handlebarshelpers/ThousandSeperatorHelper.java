e481c3b242c0823348b196228ea0a45dadf70539
Michal J. Sladek <michal.sladek@nav.no>
2022-01-11 16:46:09 +0100

    Legger på norsk locale.

--- src/test/java/no/nav/foreldrepenger/dokgen/test/handlebarshelpers/ThousandSeperatorHelper.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/handlebarshelpers/ThousandSeperatorHelper.java
@@ -1,12 +1,13 @@
 package no.nav.foreldrepenger.dokgen.test.handlebarshelpers;
 
+import java.util.Locale;
+
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 
 public class ThousandSeperatorHelper implements Helper<Integer> {
     @Override
     public Object apply(Integer kroner, final Options options) {
-        return String.format("%,d", kroner);
-
+        return String.format(Locale.forLanguageTag("NO"), "%,d", kroner);
     }
 }
