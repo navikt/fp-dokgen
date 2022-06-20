5511aec270060a4cd58557e78b9cdde974ea242c
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-06-20 09:02:37 +0200

    TFP-4985: Teksten skal komme kun da mor er ufør. (#564)
    * TFP-4985: Tessten skal komme kun da mor er ufør.
    * Bedre beskrivelse av tester. Tydeligjørt tester før og etter WLB.

--- src/test/java/no/nav/foreldrepenger/dokgen/test/templates/BfhrMorUførTest.java
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/BfhrMorUførTest.java
@@ -2,6 +2,8 @@ package no.nav.foreldrepenger.dokgen.test.templates;
 
 import no.nav.foreldrepenger.dokgen.test.support.Brevmal;
 import no.nav.foreldrepenger.dokgen.test.support.Språk;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.compileContent;
@@ -13,37 +15,86 @@ class BfhrMorUførTest {
     private static final Brevmal BREVMAL = Brevmal.FORELDREPENGER_INNVILGELSE;
 
     @Test
-    void med_og_uten_aktkrav() throws Exception {
+    @DisplayName("Mor er ufør, med og uten aktivitetskrav dager, barn født etter 02.08.2022 (etter WLB).")
+    /*
+     *   "disponibleDager": 110,
+     *   "disponibleDagerUtenAktivitetskrav": 75,
+     *   "disponibleDagerMedAktivitetskrav": 35,
+     *   "utenMinsterett": false,
+     */
+    void med_og_uten_aktkrav_etter_wlb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_med_og_uten_aktkrav");
         var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_med_og_uten_aktkrav.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    void uten_aktkrav() throws Exception {
+    @DisplayName("Mor er ufør, med og uten aktivitetskrav dager, barn født før 02.08.2022 (før WLB).")
+    /*
+     *   "disponibleDager": 110,
+     *   "disponibleDagerUtenAktivitetskrav": 75,
+     *   "disponibleDagerMedAktivitetskrav": 35,
+     *   "utenMinsterett": true,
+     */
+    void med_og_uten_aktkrav_før_wlb() throws Exception {
+        var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_med_og_uten_aktkrav_uten_minsterett");
+        var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav_uten_minsterett.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    @DisplayName("Mor er ufør, uten aktivitetskrav dager, barn født etter 02.08.2022 (etter WLB).")
+    /*
+     *   "disponibleDager": 75,
+     *   "disponibleDagerUtenAktivitetskrav": 75,
+     *   "disponibleDagerMedAktivitetskrav": 0,
+     *   "utenMinsterett": false,
+     */
+    void uten_aktkrav_etter_wlb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_uten_aktkrav");
         var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_uten_aktkrav.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    void uten_aktkrav_med_misterett() throws Exception {
+    @DisplayName("Mor er ufør, uten aktivitetskrav dager, barn født før 02.08.2022 (før WLB).")
+    /*
+     *   "disponibleDager": 75,
+     *   "disponibleDagerUtenAktivitetskrav": 75,
+     *   "disponibleDagerMedAktivitetskrav": 0,
+     *   "utenMinsterett": true,
+     */
+    void uten_aktkrav_før_wlb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_uten_aktkrav_uten_minsterett");
         var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_uten_aktkrav_uten_minsterett.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    void med_aktkrav() throws Exception {
+    @DisplayName("Mor er ufør, med aktivitetskrav dager, barn født etter 02.08.2022 (etter WLB).")
+    /*
+     *   "disponibleDager": 75,
+     *   "disponibleDagerUtenAktivitetskrav": 0,
+     *   "disponibleDagerMedAktivitetskrav": 75,
+     *   "utenMinsterett": false,
+     */
+    void med_aktkrav_etter_wlb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav");
         var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 
     @Test
-    void med_aktkrav_med_minsterett() throws Exception {
+    @DisplayName("Mor er ufør, med aktivitetskrav dager, barn født før 02.08.2022 (før WLB).")
+    /*
+     *   "disponibleDager": 75,
+     *   "disponibleDagerUtenAktivitetskrav": 0,
+     *   "disponibleDagerMedAktivitetskrav": 75,
+     *   "utenMinsterett": true,
+     */
+    void med_aktkrav_før_wlb() throws Exception {
         var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav_uten_minsterett");
-        var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav_uten_minsterett.txt");
+        var expected = getExpected(BREVMAL, "forstegangsbehandling/bfhr_mor_ufør_med_aktkrav.txt");
         assertThat(content).isEqualToIgnoringNewLines(expected);
     }
 }
