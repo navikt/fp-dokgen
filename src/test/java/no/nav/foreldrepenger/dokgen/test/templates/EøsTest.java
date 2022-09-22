a45d19a817e1d6cb458427e89e9affd34323b82d
Anders Palfi <anderspalfi@gmail.com>
2022-09-22 13:58:37 +0200

    TFP-5184 tekst i innvilgebrevet der annen forelder har rett i eøs

---
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/EøsTest.java
@@ -0,0 +1,20 @@
+package no.nav.foreldrepenger.dokgen.test.templates;
+
+import no.nav.foreldrepenger.dokgen.test.support.Brevmal;
+import no.nav.foreldrepenger.dokgen.test.support.Språk;
+import org.junit.jupiter.api.Test;
+
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.compileContent;
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.getExpected;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EøsTest {
+    private static final Brevmal BREVMAL = Brevmal.FORELDREPENGER_INNVILGELSE;
+
+    @Test
+    void far_der_mor_har_rett_eøs() throws Exception {
+        var content = compileContent(BREVMAL, Språk.BOKMÅL, "forstegangsbehandling/far_der_mor_rett_eøs");
+        var expected = getExpected(BREVMAL, "forstegangsbehandling/far_der_mor_rett_eøs_nb.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+}
