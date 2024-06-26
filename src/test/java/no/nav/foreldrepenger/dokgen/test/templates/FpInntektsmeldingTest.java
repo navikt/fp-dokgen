cc33b9e717c994cd6d7b092a3cafc3da7b717187
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-06-26 16:36:30 +0200

    Inntektsmeldingsmal for generering av pdf

---
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/FpInntektsmeldingTest.java
@@ -0,0 +1,21 @@
+package no.nav.foreldrepenger.dokgen.test.templates;
+
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.compileContent;
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.getExpected;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.dokgen.test.support.Brevmal;
+import no.nav.foreldrepenger.dokgen.test.support.Språk;
+
+class FpInntektsmeldingTest {
+    private static final Brevmal BREVMAL = Brevmal.FP_INNTEKTSMELDING;
+
+    @Test
+    void alleFelterIInntektsmeldingen() {
+        var content = compileContent(BREVMAL, Språk.BOKMÅL, "inntektsmeldingMedAllleFelter");
+        var expected = getExpected(BREVMAL, "alle-felter-i-inntektsmeldingen.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+}
