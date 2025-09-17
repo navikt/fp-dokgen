b1e1ccebb731895a5d1be0a7356c05fee21b6d17
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2025-09-17 09:32:31 +0200

    TFP-6386 foreldrepenger-innvilgelse (#833)
    * TFP-6386 foreldrepenger-innvilgelse
    Legger inn egen undermal i foreldrepenger innvilgelsesbrevet for å håndtere vedtak av revurdering grunnet aap praksisendringstilfeller tilbake i tid.

---
+++ src/test/java/no/nav/foreldrepenger/dokgen/test/templates/innvilgelsefp/ForeldrepengerInnvilgelseAapPraksisendringTest.java
@@ -0,0 +1,22 @@
+package no.nav.foreldrepenger.dokgen.test.templates.innvilgelsefp;
+
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.compileContent;
+import static no.nav.foreldrepenger.dokgen.test.support.TemplateTestService.getExpected;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.dokgen.test.support.Brevmal;
+import no.nav.foreldrepenger.dokgen.test.support.Språk;
+
+class ForeldrepengerInnvilgelseAapPraksisendringTest {
+    private static final Brevmal BREVMAL = Brevmal.FORELDREPENGER_INNVILGELSE;
+    private static final String UNDERMAL = "aap_praksisendring";
+
+    @Test
+    void skal_informere_om_aap_praksisk_endring_nb() {
+        var content = compileContent(BREVMAL, UNDERMAL, Språk.BOKMÅL, "foreldrepenger_aap_praksisendring");
+        var expected = getExpected(BREVMAL, UNDERMAL, "foreldrepenger_aap_praksisendring_nb.txt");
+        assertThat(content).isEqualToIgnoringNewLines(expected);
+    }
+}
