c192c383ca50150b0e7fd22ade23734750feba9c
Michal J. Sladek <michal.sladek@nav.no>
2026-02-11 11:01:50 +0100

    Code formatering og imports

--- src/main/java/no/nav/foreldrepenger/dokgen/server/exceptions/JsonMappingExceptionMapper.java
+++ src/main/java/no/nav/foreldrepenger/dokgen/server/exceptions/JsonMappingExceptionMapper.java
@@ -1,15 +1,14 @@
 package no.nav.foreldrepenger.dokgen.server.exceptions;
 
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-
-import jakarta.ws.rs.ext.Provider;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
